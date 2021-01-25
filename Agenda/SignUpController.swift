96a7f9248b1fda629a7f33ee8430fe58e2392a99
JoseAlejandroGarciaCifuentes <jose_alejandro_garcia_apps1ma1920@cev.com>
2021-01-25 21:59:06 +0100

    git init and sign up view

---
+++ Agenda/SignUpController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Agenda
+//
+//  Created by Alejandro García on 25/1/21.
+//  Copyright © 2021 Alejandro García. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
