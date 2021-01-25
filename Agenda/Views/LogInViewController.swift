46670e16b55d5da622d36d3c4c3c62e3914d7cdd
JoseAlejandroGarciaCifuentes <jose_alejandro_garcia_apps1ma1920@cev.com>
2021-01-25 23:54:09 +0100

    alamofire installed, sign up view new colors

--- Agenda/Views/LogInViewController.swift
+++ Agenda/Views/LogInViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LogInController: UIViewController {
+class LogInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Agenda/Views/SignUpViewController.swift
+++ Agenda/Views/SignUpViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SignUpController: UIViewController {
+class SignUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
